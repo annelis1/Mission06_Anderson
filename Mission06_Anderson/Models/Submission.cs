46c0930335faffd8f7808bf8722552c978e07086
annelis1 <119529206+annelis1@users.noreply.github.com>
2024-02-16 21:58:54 -0700

    Finalizing

--- Mission06_Anderson/Models/Submission.cs
+++ Mission06_Anderson/Models/Submission.cs
@@ -7,13 +7,29 @@ namespace Mission06_Anderson.Models
         [Key]
         [Required]
         public int SubmissionId { get; set; }
+
+        
+        [Required]
         public string Category { get; set; }
         public string? Category2 { get; set; }
+
+        
+        [Required]
         public string Title { get; set; }
+
+        
+        [Required]
         public int Year { get; set; }
+
+        
+        [Required]
         public string Director { get; set; }
+
+        [Required]
         public string Rating { get; set; }
+
         public bool? Edited { get; set; }
+
         public string? Lent { get; set; }
         
         //limit string length and send error message if exceeded
